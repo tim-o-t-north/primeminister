ad684ebc7dc02eea54ce7ecb285d6173e93c1bd3
Tim Pillinger <tim.pillinger@metoffice.gov.uk>
2017-06-18 08:29:30 +0100

    Main Mrs May Product

---
+++ mrsmay.py
@@ -0,0 +1,12 @@
+def mrs_may():
+    print "Hello, I am your Prime-Minister"
+    n=0    
+    while n < 3:    
+        answer = raw_input("Did you want to ask me somthing?  ")
+        print "Strong and Stable"
+        n +=1
+    print "Brexit Means Brexit!"
+    raise TypeError("Tautology error: 'Brexit Means Brexit is undefined")
+
+
+mrs_may()
