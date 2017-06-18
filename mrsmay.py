66501a27b0f571da804f3ae0b07cafe04a05f481
Tim Pillinger <tim.pillinger@metoffice.gov.uk>
2017-06-18 08:39:09 +0100

    code checks on changeds in ad684eb

--- mrsmay.py
+++ mrsmay.py
@@ -1,12 +1,19 @@
 def mrs_may():
+    """
+    A subroutine simulation of the UK Prime-Minister
+    Teresa May.
+    For further help see
+    https://en.wikipedia.org/wiki/Satire
+    """
     print "Hello, I am your Prime-Minister"
-    n=0    
-    while n < 3:    
+    counter = 0
+    while counter < 3:
         answer = raw_input("Did you want to ask me somthing?  ")
         print "Strong and Stable"
-        n +=1
+        counter += 1
     print "Brexit Means Brexit!"
     raise TypeError("Tautology error: 'Brexit Means Brexit is undefined")
 
 
-mrs_may()
+if __name__ == "__main__":
+    mrs_may()
